4278da7b32969d0068a1199977542bb8f92ee0ae
Godwin <godwinmail6@gmail.com>
2023-11-08 14:37:53 +0100

    NotFound Exception found

---
+++ src/main/java/Exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(int id){
+        super("This element with id " + id + " was not found");
+    }
+}
