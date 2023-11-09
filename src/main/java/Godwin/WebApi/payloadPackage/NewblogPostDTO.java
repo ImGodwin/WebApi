817c4c4b07fe9de15d866730e980c15f1c9a7845
Godwin <godwinmail6@gmail.com>
2023-11-09 18:14:14 +0100

    completed blogs dto

--- src/main/java/Godwin/WebApi/payloadPackage/NewblogPostDTO.java
+++ src/main/java/Godwin/WebApi/payloadPackage/NewblogPostDTO.java
@@ -1,5 +1,12 @@
 package Godwin.WebApi.payloadPackage;
 
-public record NewblogPostDTO(String categories, String title, String cover,
-                             String content, int readingTime, int author_id) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record NewblogPostDTO(@NotEmpty(message = "Please add a category") @Size(min = 6, max =15) String categories,
+                             @NotEmpty(message = "Please add a title") @Size(min = 2, max =30)String title,
+                             @NotEmpty(message = "Please add a cover") String cover,
+                             @NotEmpty(message = "Please add a content") String content,
+                             @NotEmpty(message = "Please add an author reading time") int readingTime,
+                             @NotEmpty(message = "Please add an author ID")int author_id) {
 }
